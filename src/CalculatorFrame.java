561324f3dec839b76f05e4611949838ca482994a
Egor Lishtvan <Lishtvan>
2022-02-18 03:52:22 +0300

    Initial commit

---
+++ src/CalculatorFrame.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+
+public class CalculatorFrame extends JFrame {
+    CalculatorFrame() {
+        int w = 270, h = 240;
+        setTitle("Калькулятор");
+        setBounds(100, 100, w, h);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        CalcPanel panel = new CalcPanel(w, h);
+        add(panel);
+        setResizable(false);
+        setVisible(true);
+    }
+}
+class CalcPanel extends JPanel{
+    public JTextField TextField;
+    private BtnAction BtnPressed;
+    CalcPanel(int W, int H){
+        int w=W/5, h=H/8,sx=w/5, sy=h/3;
+        setLayout(null);
+        setBorder(BorderFactory.createEtchedBorder());
+        JTextField TxtFld=new JTextField();
+        TxtFld.setHorizontalAlignment(JTextField.RIGHT);
+        TxtFld.setBounds(sx,sy,2*sx+3*w,h);
+        TxtFld.setEditable(false);
+        add(TxtFld);
+        BtnPressed=new BtnAction(TxtFld);
+        String[] BtnTxt={
+                "1","2","3","+","4","5","6","-","7","8","9","/",
+                "0",".","=","*"
+        };
+        for (int i=0;i<BtnTxt.length;i++){
+            addBtn(sx+(w+sx)*(i%4),(2*sy+h)+(sy+h)*(i/4),w,h,BtnTxt[i],BtnPressed);
+        }
+    }
+}
